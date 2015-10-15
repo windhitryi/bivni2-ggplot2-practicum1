adc8163c6303edb09305082cf5fed5b332c68232
unknown <windhittyi@gmail.com>
2015-10-15 19:02:08 +0300

    сделала три графика, третий не совсем до конца доделала(без subset)

--- lab_part2_colorado_ggplot2_lab.R
+++ lab_part2_colorado_ggplot2_lab.R
@@ -9,3 +9,14 @@ library(ggplot2)
 
 head(film_death_counts)
 
+
+
+ggplot()+ geom_point(data=film_death_counts,mapping = aes(x=Year, y=IMDB_Rating, 
+                                                  color=MPAA_Rating)) + ylab("Average IMDB Rating") 
+
+ggplot()+  geom_smooth(data=film_death_counts,mapping = aes(x=Year, y=IMDB_Rating))+ ylab("Average IMDB Rating")
+                                                           
+
+ggplot(data = film_death_counts, aes(x=Body_Count))+ geom_histogram()+ 
+  facet_grid(.~MPAA_Rating)+  scale_y_sqrt()+ 
+  xlab("Number of Deaths")+  ylab("Number of Films")
